a00d0f3f57f76d2f082709e87e24fe6613a14e9c
HaithamSanari <haithamsanari@gmail.com>
2022-07-29 16:38:25 +0300

    Add structure code

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $('#submit').click(function (e) {
+    e.preventDefault()
+    let fName = $('#fName').val()
+    let lName = $('#lName').val()
+    let phone = $('#phone').val()
+    let email = $('#email').val()
+    let message = $('#message').val()
+    let validEmail =
+      /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
+    $('#fName, #lName, #phone, #email, #message').removeClass('input-error')
+    if (fName == '' || lName == '' || email == '') {
+      $('#fName, #lName, #email').addClass('input-error')
+      $('#error_message').html('All Fields are required!')
+    } else if (!validEmail.test(email)) {
+      $('#email').addClass('input-error')
+      $('#error_message').html('Write a valid e-mail address!')
+    } else {
+      $('#error_message').html('')
+      $.ajax({
+        url: 'https://formsubmit.co/ajax/haithamsanari@gmail.com',
+        method: 'POST',
+        data: {
+          fName: fName,
+          lName: lName,
+          phone: phone,
+          email: email,
+          message: message,
+        },
+        success: function (data) {
+          $('form').trigger('reset')
+          $('#success_message').fadeIn().html('Thank you for submitting', data)
+          setTimeout(function () {
+            $('#success_message').fadeOut('Slow')
+          }, 4000)
+        },
+      })
+    }
+  })
+})
